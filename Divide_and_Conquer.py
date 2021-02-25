3876de89a4ffe9dcefd9cbd0a7dcaac88ef0b1d7
Nikita <Narchuk96@gmail.com>
2021-02-25 21:34:42 +0300

    new

--- Divide_and_Conquer.py
+++ Divide_and_Conquer.py
@@ -15,7 +15,6 @@ def div_con(x):
     return (sum(lst_int) - sum(lst_str))
 
 
-print(div_con([9, 3, '7', '3']))
 print(div_con(['5', '0', 9, 3, 2, 1, '9', 6, 7]))
 print(div_con(['3', 6, 6, 0, '5', 8, 5, '6', 2,'0']))
 print(div_con(['1', '5', '8', 8, 9, 9, 2, '3']))
